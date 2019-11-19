c32965bf7ea66fa75738aae6308594142d5a079a
naman <f2014727@pilani.bits-pilani.ac.in>
2019-11-19 12:27:38 +0530

    checking notebooks

--- manage.py
+++ manage.py
@@ -1,10 +1,111 @@
-#!/usr/bin/env python
-import os
-import sys
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+{
+  "paragraphs": [
+    {
+      "text": "%md",
+      "user": "namanu@qubole.com",
+      "dateUpdated": "Nov 19, 2019 6:52:13 AM",
+      "config": {
+        "colWidth": 12.0,
+        "graph": {
+          "mode": "table",
+          "height": 300.0,
+          "optionOpen": false,
+          "keys": [],
+          "values": [],
+          "groups": [],
+          "scatter": {}
+        },
+        "enabled": true
+      },
+      "settings": {
+        "params": {},
+        "forms": {}
+      },
+      "version": "v0",
+      "jobName": "paragraph_1574142435281_1378078064",
+      "id": "20191119-054715_1926594556_q_1ANMCNVVGX1574142406",
+      "dateCreated": "Nov 19, 2019 5:46:46 AM",
+      "status": "READY",
+      "progressUpdateIntervalMs": 1000
+    },
+    {
+      "text": "\n\n",
+      "user": "namanu@qubole.com",
+      "dateUpdated": "Nov 19, 2019 6:50:42 AM",
+      "config": {
+        "colWidth": 12.0,
+        "graph": {
+          "mode": "table",
+          "height": 300.0,
+          "optionOpen": false,
+          "keys": [],
+          "values": [],
+          "groups": [],
+          "scatter": {}
+        },
+        "enabled": true,
+        "editorMode": "ace/mode/scala"
+      },
+      "settings": {
+        "params": {},
+        "forms": {}
+      },
+      "version": "v1",
+      "jobName": "paragraph_1574145107599_-1902309147",
+      "id": "20191119-063147_1728381593_q_1ANMCNVVGX1574142406",
+      "result": {
+        "code": "ERROR",
+        "type": "TEXT",
+        "msg": "\ngzip: stdin: unexpected end of file\ntar: Child returned status 1\ntar: Error is not recoverable: exiting now\nLast login: Tue Nov 19 06:49:19 UTC 2019\nExitValue: 2"
+      },
+      "dateCreated": "Nov 19, 2019 6:31:47 AM",
+      "dateSubmitted": "Nov 19, 2019 6:50:31 AM",
+      "dateStarted": "Nov 19, 2019 6:50:31 AM",
+      "dateFinished": "Nov 19, 2019 6:50:31 AM",
+      "status": "ERROR",
+      "progressUpdateIntervalMs": 1000
+    },
+    {
+      "text": "",
+      "config": {},
+      "settings": {
+        "params": {},
+        "forms": {}
+      },
+      "version": "v0",
+      "jobName": "paragraph_1574145225217_2030963534",
+      "id": "20191119-063345_597701989_q_1ANMCNVVGX1574142406",
+      "dateCreated": "Nov 19, 2019 6:33:45 AM",
+      "status": "READY",
+      "progressUpdateIntervalMs": 1000
+    },
+    {
+      "text": "",
+      "config": {},
+      "settings": {
+        "params": {},
+        "forms": {}
+      },
+      "version": "v0",
+      "jobName": "paragraph_1574146124292_786592525",
+      "id": "20191119-064844_1190339494_q_1ANMCNVVGX1574142406",
+      "dateCreated": "Nov 19, 2019 6:48:44 AM",
+      "status": "READY",
+      "progressUpdateIntervalMs": 1000
+    }
+  ],
+  "name": "naman",
+  "id": "1ANMCNVVGX1574142406",
+  "angularObjects": {
+    "2EVB7TESB907771574143125480:shared_process": [],
+    "2EVE87CDT907771574143125480:shared_process": [],
+    "2EVGC1QAY907771574143125481:shared_process": [],
+    "2EVPKUY7Y907771574143125374:shared_process": []
+  },
+  "config": {
+    "isDashboard": false,
+    "defaultLang": "spark"
+  },
+  "info": {},
+  "source": "FCN"
+}
